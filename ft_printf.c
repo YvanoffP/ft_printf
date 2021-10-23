3637efa0268fafd3b4f20f267a6ec14029e57cbc
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-23 20:09:11 +0200

    first draw

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,18 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 17:56:48 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/10/23 17:58:19 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/10/23 20:08:48 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *arg ...)
 {
+	const char	*current_arg;
+	va_list		arguments;
 
+	va_start(arguments, first);
+	current_arg = va_arg(arguments, const char *); // arguments pointer is ++ when this is called
+
+	// il faut un compteur de % pour pouvoir definir combien d'arguments sont passes
+	
 }
