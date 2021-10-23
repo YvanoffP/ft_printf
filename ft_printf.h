2a934db21359bd20745d4598960fff673e0d47b6
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-10-23 19:05:09 +0200

    stdarg

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,16 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 17:58:31 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/10/23 18:00:13 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/10/23 19:04:47 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdio.h>
+# include <stdio.h>
+# include "libft.h"
+# include <stdarg.h>
 
 int	ft_printf(const char *, ...);
 
