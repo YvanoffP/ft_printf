a7cfac382c061b81671a803562027b0fcde55f70
YvanoffP <petruzzi.yvan@gmail.com>
2021-10-26 21:07:35 +0200

    big push

---
+++ src/ft_nbrlen.c
@@ -0,0 +1,20 @@
+#include "printf.h"
+#include <stdio.h>
+
+size_t	ft_nbrlen(long long int nbr)
+{
+	size_t	count;
+
+	count = 0;
+	if (nbr < 0)
+	{
+		count++;
+		nbr = nbr * -1;
+	}
+	while (nbr > 0)
+	{
+		nbr = nbr / 10;
+		count++;
+	}
+	return (count - 1);
+}
